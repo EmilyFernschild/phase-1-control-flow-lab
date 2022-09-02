fcb4b36fc01a97b07f50d30ada2b87eaae217bc1
Emily Fernschild <emilyfernschild@gmail.com>
2022-09-02 12:37:32 -0400

    commit tree 9ee26ed8c98ba0f1ff237e7d95743745e2408cb2

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue < 200){
+    result = 'This one is on me!';
+  }
+  else if (someValue > 400 && someValue < 2000){
+    result = 'That will be twenty bucks.';
+  }
+  else if (someValue > 2000){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (someValue > 2000 && someValue > 2500){
+    result = 'No can do.';
+  }
+  return result
+}
+ 
+function ternaryCheckCity(city){
+        const anyCity  = city;
+        return anyCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
+    //     let anyCity = "NYC";
+    
+    // if (anyCity === true){
+    //  city = 'Ok, sounds good.';
+    // }
+    // else if (anyCity === false) {
+    //   city = 'No go.';
+    // }
+
+    //return city;
+
+   
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(charm){
+  switch (charm) {
+    case 'generous':
+      return ('Thank you so much.');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'not as generous':
+      return ('Thank you.');
+
+    case 'thanks for everything':
+      return ('Bye.');
+  }
 }
